06f9df3401dbf88a2336efb0d7cf66bd0700f83c
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2020-11-21 23:07:51 -0500

    please work

---
+++ routes/htmlRoute.js
@@ -0,0 +1,12 @@
+var router = require("express").Router();
+var path = require("path");
+
+
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(_dirname, "../public/notes.html"))
+})
+
+router.get("*", function (req, res) {
+    res.sendFile(path.join(_dirname, "../public/index.html"))
+})
+module.exports = router
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.dynamic("public"));
 
 app.use("/api", apiRoutes);
 
