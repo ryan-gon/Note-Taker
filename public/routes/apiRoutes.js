5515bf0951cdf9b962c596d95e9379b3f8d7e3b7
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2020-11-21 22:16:17 -0500

    clean up

--- public/routes/apiRoutes.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const router = require("express").Router();
-const dbJSON = require("../db/db.json")
-
-
-// JSON file read
-router.get("/api/notes", function (req, res) {
-  res.json(dbJSON);
-});
